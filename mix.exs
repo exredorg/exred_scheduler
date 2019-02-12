de2f9b971df93f728193173c99748a1ee61c48e6
Zsolt <zsolt.erl@gmail.com>
2019-02-11 19:11:11 -0500

    updated deps

--- mix.exs
+++ mix.exs
@@ -47,7 +47,6 @@ defmodule Exred.Scheduler.Mixfile do
       {:exred_node_aws_iot_thingshadow_out,
        git: "https://github.com/exredorg/exred_node_aws_iot_thingshadow_out.git"},
       {:exred_node_debug, git: "https://github.com/exredorg/exred_node_debug.git"},
-      {:exred_node_debug, path: "../exred_node_debug"},
       {:exred_node_gpio_in, git: "https://github.com/exredorg/exred_node_gpio_in.git"},
       {:exred_node_gpio_out, git: "https://github.com/exredorg/exred_node_gpio_out.git"},
       {:exred_node_redis_daemon, git: "https://github.com/exredorg/exred_node_redis_daemon.git"},
@@ -55,7 +54,6 @@ defmodule Exred.Scheduler.Mixfile do
       {:exred_node_redis_out, git: "https://github.com/exredorg/exred_node_redis_out.git"},
       {:exred_node_suppress, git: "https://github.com/exredorg/exred_node_suppress.git"},
       {:exred_node_trigger, git: "https://github.com/exredorg/exred_node_trigger.git"},
-      {:exred_node_trigger, path: "../exred_node_trigger"},
       {:exred_node_rpiphoto, git: "https://github.com/exredorg/exred_node_rpiphoto.git"},
       {:exred_node_shell, git: "https://github.com/exredorg/exred_node_shell.git"},
       {:exred_node_picar, git: "https://github.com/exredorg/exred_node_picar.git"},
@@ -68,7 +66,7 @@ defmodule Exred.Scheduler.Mixfile do
   defp nodes(_) do
     [
       {:exred_library, "~> 0.1"},
-      {:exred_node_aws_iot_daemon, "~> 0.1.0"},
+      {:exred_node_aws_iot_daemon, "~> 0.2.0"},
       {:exred_node_aws_iot_thingshadow_in, "~> 0.2.0"},
       {:exred_node_aws_iot_thingshadow_out, "~> 0.2.0"},
       {:exred_node_debug, "~> 0.2.0"},
